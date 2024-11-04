968974d8763ca73502c5c59018e985f68d8ea34e
ijun17 <ijun17@naver.com>
2024-11-04 18:20:26 +0900

    feat: 게임 시작 및 설정 페이지 구현

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { MainPage } from './pages/MainPage';
+import { GameSetupPage } from './pages/GameSetupPage';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<MainPage />} />
+        <Route path="/game/setup" element={<GameSetupPage />} />
         <Route path="*" element={<div>not found</div>} />
       </Routes>
     </Router>
