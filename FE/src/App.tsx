3ab38c448d37625558f9e871c48787a3403c6ec8
ijun17 <ijun17@naver.com>
2024-11-04 17:13:50 +0900

    feat: MainPage 구현 및 라우팅

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -1,5 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { MainPage } from './pages/MainPage';
+
 function App() {
-  return <div></div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="*" element={<div>not found</div>} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ FE/src/pages/MainPage.tsx
@@ -0,0 +1,10 @@
+export const MainPage = () => {
+  return (
+    <>
+      <button>게임방 만들기</button>
+      <button>대기방 목록</button>
+      <button>PIN으로 방찾기</button>
+      <button>로그인</button>
+    </>
+  );
+};
