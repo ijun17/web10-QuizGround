6d7bc85987991b1f414b3862774d05a30e1e88c3
ijun17 <ijun17@naver.com>
2024-11-05 16:52:03 +0900

    feat: tailwind.config 설정

--- FE/tailwind.config.js
+++ FE/tailwind.config.js
@@ -6,7 +6,53 @@ export default {
     "./node_modules/@mui/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        main: '#7890E7'
+      },
+      fontSize: {
+        l: '1.5rem',
+        m: '1rem',
+        r: '0.75rem',
+        s: '0.625rem',
+      },
+      textColor: {
+        default: '#5F6E76',
+        weak: '#879298'
+      },
+      backgroundColor: {
+        surface: {
+          default: 'white',
+          alt: '#F5F7F9'
+        }
+      },
+      borderColor: {
+        default: '#879298'
+      },
+      borderRadius: {
+        m: '1rem',
+        s: '0.5rem'
+      },
+      dropShadow: {
+        popup: '0 4px 2px rgba(20, 33, 43, 0.02)'
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+    function ({ addUtilities, theme }) {
+      const newUtilities = {
+        '.component-default': {
+          borderWidth: '1px',
+          borderColor: '#879298',
+          borderRadius: theme('borderRadius.m'),
+          backgroundColor: theme('backgroundColor.default'),
+        },
+        '.component-popup': {
+          borderRadius: theme('borderRadius.m'),
+          dropShadow: theme('dropShadow.popup'),
+        },
+      };
+      addUtilities(newUtilities, ['responsive', 'hover']);
+    }
+  ],
 }
