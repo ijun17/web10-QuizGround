f014eca1fe148af80f8c1b80f26e0eb573e58b21
DongHoonYu <50190387+DongHoonYu96@users.noreply.github.com>
2024-11-06 11:27:05 +0900

    chore: [BE] 안쓰는 모듈삭제

--- BE/src/app.module.ts
+++ BE/src/app.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GameModule } from './game/game.module';
-import { ChatsModule } from './chats/chats.module';
 
 @Module({
-  imports: [GameModule, ChatsModule],
+  imports: [GameModule],
   controllers: [AppController],
   providers: [AppService],
 })
