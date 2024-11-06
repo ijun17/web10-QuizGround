e398d0122355274185a78e62f8f8c30ed2ac9754
ijun17 <ijun17@naver.com>
2024-11-06 22:18:50 +0900

    feat: 서버로 부터 받은 실제 참가자 표시

--- FE/src/components/ParticipantDisplay.tsx
+++ FE/src/components/ParticipantDisplay.tsx
@@ -1,15 +1,18 @@
-const samplePalyer = Array(100)
-  .fill(null)
-  .map((_, i) => ({ id: i, name: 'user' + i }));
+import { usePlayerStore } from '@/store/usePlayerStore';
 
 const ParticipantDisplay = () => {
+  const players = usePlayerStore((state) => state.players);
+  console.log(players);
   return (
     <div className="component-default h-[100%]">
       <div className="border-b border-default center h-[2.5rem]">참가자</div>
       <div className="p-3 h-[calc(100%-2.5rem)] overflow-y-scroll">
-        {samplePalyer.map((e, i) => (
-          <div className="flex justify-between mt-2 pb-2 border-b border-default" key={e.id}>
-            <div className="font-bold">{i + 1 + '. ' + e.name}</div>
+        {players.map((player, i) => (
+          <div
+            className="flex justify-between mt-2 pb-2 border-b border-default"
+            key={player.playerId}
+          >
+            <div className="font-bold">{i + 1 + '. ' + player.playerName}</div>
             <button className="bg-main rounded-lg text-white w-8 h-6 text-r">강퇴</button>
           </div>
         ))}
