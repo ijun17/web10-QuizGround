0b6f2e9d2522e0effc628c6fd761d1ce8ab51c3f
songbuild00 <songbuild00@gmail.com>
2024-11-07 18:12:53 +0900

    refactor: 이후를 위해 미리 entities 옵션 추가
    추후 Entity가 추가되는 경우 해당 옵션에 입력 필요

--- BE/src/app.module.ts
+++ BE/src/app.module.ts
@@ -17,6 +17,7 @@ dotenv.config({ path: '../.env' });
       username: process.env.DB_USER,
       password: process.env.DB_PASSWD,
       database: process.env.DB_NAME,
+      entities: [],
       synchronize: false // 개발 모드에서만 활성화
     })
   ],
