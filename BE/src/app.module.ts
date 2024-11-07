753fdc83bcf5154778a801f007313d38d25eca2e
songbuild00 <songbuild00@gmail.com>
2024-11-07 17:25:02 +0900

    feat: TypeORM 초기 세팅

--- BE/src/app.module.ts
+++ BE/src/app.module.ts
@@ -2,9 +2,24 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GameModule } from './game/game.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
+
+dotenv.config({ path: '../.env' });
 
 @Module({
-  imports: [GameModule],
+  imports: [
+    GameModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: 3306,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWD,
+      database: process.env.DB_NAME,
+      synchronize: true
+    })
+  ],
   controllers: [AppController],
   providers: [AppService]
 })
