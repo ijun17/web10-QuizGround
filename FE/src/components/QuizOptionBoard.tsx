ddd4f9009ea06f0c3da12a4d49fffb6d000fd692
ijun17 <ijun17@naver.com>
2024-11-12 22:22:24 +0900

    feat: QuizOptionBoard 테스트 데이터 적용

--- FE/src/components/QuizOptionBoard.tsx
+++ FE/src/components/QuizOptionBoard.tsx
@@ -3,9 +3,7 @@ import { Player } from './Player';
 import { socketService } from '@/api/socket';
 import { useRoomStore } from '@/store/useRoomStore';
 import { useState } from 'react';
-type Params = {
-  options: string[];
-};
+import { useQuizeStore } from '@/store/useQuizStore';
 
 const optionColors = [
   '#FF9AA2', // pastel red
@@ -20,21 +18,36 @@ const optionColors = [
   '#C3B3E0' // pastel purple
 ];
 
-export const QuizOptionBoard = ({ options }: Params) => {
+const mockQuiz = {
+  quiz: '이것은 목데이터 퀴즈입니다.',
+  choiceList: [
+    { content: '옵션 1', order: 1 },
+    { content: '옵션 2', order: 2 },
+    { content: '옵션 3', order: 3 },
+    { content: '옵션 4', order: 4 }
+  ],
+  endTime: Date.now() + 10000 // 10초 후 종료
+};
+
+export const QuizOptionBoard = () => {
   const currentPlayerId = usePlayerStore((state) => state.currentPlayerId);
   const gameId = useRoomStore((state) => state.gameId);
   const players = usePlayerStore((state) => state.players);
+  // const options = useQuizeStore((state) => state.currentQuiz?.choiceList) || [];
+  const options = useQuizeStore((state) => state.currentQuiz?.choiceList) || mockQuiz.choiceList;
   const [selectedOption, setSelectedOption] = useState(options.length);
+
   const handleClick: React.MouseEventHandler<HTMLDivElement> = (e) => {
     const { pageX, pageY } = e;
     const { width, height, top, left } = e.currentTarget.getBoundingClientRect();
     const x = (pageX - left) / width;
     const y = (pageY - top) / height;
+    if (x > 1 || y > 1) return;
     socketService.emit('updatePosition', { gameId, newPosition: [y, x] });
     const option = Math.round(x) + Math.floor(y * Math.ceil(options.length / 2)) * 2;
     setSelectedOption(option);
-    console.log(option);
   };
+
   return (
     <div className="relative component-default h-[100%]" onClick={handleClick}>
       <div className="absolute h-[100%] w-[100%]">
@@ -57,7 +70,7 @@ export const QuizOptionBoard = ({ options }: Params) => {
               border: 'solid 3px ' + (i === selectedOption ? 'lightgreen' : 'white')
             }}
           >
-            {i + 1 + '. ' + option}
+            {i + 1 + '. ' + option.content}
           </div>
         ))}
       </div>
