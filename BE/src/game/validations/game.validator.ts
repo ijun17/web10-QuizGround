fa6ab7aa4616b0926a4c6f1f4db066db115556ed
NewCodes7 <newcodes7@gmail.com>
2024-11-12 22:02:29 +0900

    feat: 기존 게임방 관련 소켓 로직들 Redis 사용하는 걸로 변경
    - 서비스 레이어로 분리
    - validator 클래스로 역할 분리 (데코레이터 사용x)
    - 기존에 쓰던 타입 삭제

---
+++ BE/src/game/validations/game.validator.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@nestjs/common';
+import { GameWsException } from '../../common/exceptions/game.ws.exception';
+import { ExceptionMessage } from '../../common/constants/exception-message';
+
+@Injectable()
+export class GameValidator {
+  validateRoomExists(eventName: string, room: any) {
+    if (!room?.title) {
+      throw new GameWsException(
+        eventName,
+        ExceptionMessage.ROOM_NOT_FOUND
+      );
+    }
+  }
+
+  validateRoomCapacity(eventName: string, currentPlayerCount: number, maxPlayerCount: number) {
+    if (currentPlayerCount >= maxPlayerCount) {
+      throw new GameWsException(
+        eventName,
+        ExceptionMessage.ROOM_FULL
+      );
+    }
+  }
+
+  validatePlayerInRoom(eventName: string, player: any) {
+    if (!player.playerName) {
+      throw new GameWsException(
+        eventName,
+        ExceptionMessage.NOT_A_PLAYER
+      );
+    }
+  }
+}
