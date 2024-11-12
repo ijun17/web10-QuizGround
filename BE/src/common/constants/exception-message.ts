1f951af4410960afd1dea15981e9d60d75d45436
NewCodes7 <newcodes7@gmail.com>
2024-11-12 21:57:16 +0900

    feat: 예외 메시지 상수로 분리

---
+++ BE/src/common/constants/exception-message.ts
@@ -0,0 +1,7 @@
+export const ExceptionMessage = {
+  ROOM_NOT_FOUND: '존재하지 않는 게임 방입니다.',
+  ROOM_FULL: '게임 방 최대 인원이 모두 찼습니다.',
+  NOT_A_PLAYER: '해당 게임 방의 플레이어가 아닙니다.',
+  ONLY_HOST_CAN_START: '방장만 게임을 시작할 수 있습니다.',
+  GAME_NOT_STARTED: '게임이 시작되지 않았습니다.',
+};
