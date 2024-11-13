2685c204ece1cf6c796b51d712a5394a1b543513
songbuild00 <songbuild00@gmail.com>
2024-11-13 11:45:23 +0900

    refactor: 플레이어가 Host인지 확인하는 Validator 추가

--- BE/src/game/validations/game.validator.ts
+++ BE/src/game/validations/game.validator.ts
@@ -6,28 +6,25 @@ import { ExceptionMessage } from '../../common/constants/exception-message';
 export class GameValidator {
   validateRoomExists(eventName: string, room: any) {
     if (!room?.title) {
-      throw new GameWsException(
-        eventName,
-        ExceptionMessage.ROOM_NOT_FOUND
-      );
+      throw new GameWsException(eventName, ExceptionMessage.ROOM_NOT_FOUND);
     }
   }
 
   validateRoomCapacity(eventName: string, currentPlayerCount: number, maxPlayerCount: number) {
     if (currentPlayerCount >= maxPlayerCount) {
-      throw new GameWsException(
-        eventName,
-        ExceptionMessage.ROOM_FULL
-      );
+      throw new GameWsException(eventName, ExceptionMessage.ROOM_FULL);
     }
   }
 
   validatePlayerInRoom(eventName: string, player: any) {
     if (!player.playerName) {
-      throw new GameWsException(
-        eventName,
-        ExceptionMessage.NOT_A_PLAYER
-      );
+      throw new GameWsException(eventName, ExceptionMessage.NOT_A_PLAYER);
+    }
+  }
+
+  validatePlayerIsHost(eventName: string, room: any, clientId: string) {
+    if (room?.host !== clientId) {
+      throw new GameWsException(eventName, ExceptionMessage.ONLY_HOST);
     }
   }
 }
