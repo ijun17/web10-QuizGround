63adc1e4294d331b6c1eb2b4ceaee1799b36f49b
ijun17 <ijun17@naver.com>
2024-11-14 21:05:50 +0900

    feat: 퀴즈 상태 추가

--- FE/src/constants/quizState.ts
+++ FE/src/constants/quizState.ts
@@ -1,8 +1,8 @@
 const QuizState = {
-  START: 'start', // 퀴즈 시작!
+  START: 'start', // 퀴즈 시작! 3초
   PROGRESS: 'progress', // 퀴즈 진행중!
-  END: 'end', // 퀴즈 종료 !
-  WAIT: 'wait' // 퀴즈 대기중 !
+  END: 'end', // 퀴즈 종료 ! 3초
+  WAIT: 'wait' // 퀴즈 대기중 ! 퀴즈 시작 전
 } as const;
 
 export default QuizState;
