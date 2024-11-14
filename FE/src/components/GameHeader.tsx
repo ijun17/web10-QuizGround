e9120625c989ab96e8826a33229fc03c8a87f41c
ijun17 <ijun17@naver.com>
2024-11-14 22:03:46 +0900

    feat: 게임 시작 기능 구현

--- FE/src/components/GameHeader.tsx
+++ FE/src/components/GameHeader.tsx
@@ -5,18 +5,21 @@ import { useParams } from 'react-router-dom';
 import { useRoomStore } from '@/store/useRoomStore';
 import { useState } from 'react';
 import { QuizSettingModal } from './QuizSettingModal';
-import { Button } from '@mui/material';
-import AnswerModal from './AnswerModal';
+import { socketService } from '@/api/socket';
+import { usePlayerStore } from '@/store/usePlayerStore';
 
 export const GameHeader = () => {
   const { gameId } = useParams<{ gameId: string }>();
+  const isHost = usePlayerStore((state) => state.isHost);
   const gameTitle = useRoomStore((state) => state.title);
   const [isQuizModalOpen, setIsQuizModalOpen] = useState(false);
-  const [isAnswerVisible, setIsAnswerVisible] = useState(false);
   const pinNum = String(gameId);
   const linkURL = window.location.hostname + `/game/${gameId}`;
+
+  const handleStartGame = () => {
+    if (gameId) socketService.emit('startGame', { gameId });
+  };
   // 예시
-  const answer = 'A번!';
   return (
     <Card className="p-4 border border-blue-600 shadow-xl rounded-md h-[280px] w-[1000px] bg-gradient-to-b from-blue-500 to-blue-700 text-white component-popup">
       <div className="flex justify-center mb-4">
@@ -27,26 +30,23 @@ export const GameHeader = () => {
         <span className="text-xl font-semibold">{gameTitle}</span>
       </div>
       <QuizPreview title="" description="" />
-      <div className="mt-4 text-xl font-bold text-blue-500">
-        정답: {answer}
-        <Button onClick={() => setIsAnswerVisible(true)}>정답보기(개발용)</Button>
-      </div>
-      <div className="flex space-x-4 justify-center">
-        <button
-          className="bg-yellow-400 text-black font-bold py-2 px-4 rounded-md shadow-lg transform hover:translate-y-[-2px] hover:shadow-xl active:translate-y-1 active:shadow-sm transition"
-          onClick={() => setIsQuizModalOpen(true)}
-        >
-          퀴즈 설정
-        </button>
-        <button className="bg-blue-500 text-white font-bold py-2 px-4 rounded-md shadow-lg transform hover:translate-y-[-2px] hover:shadow-xl active:translate-y-1 active:shadow-sm transition">
-          게임 시작
-        </button>
-      </div>
-      <AnswerModal
-        isOpen={isAnswerVisible}
-        onClose={() => setIsAnswerVisible(false)}
-        answer={answer}
-      />
+      {isHost && (
+        <div className="flex space-x-4 justify-center">
+          <button
+            className="bg-yellow-400 text-black font-bold py-2 px-4 rounded-md shadow-lg transform hover:translate-y-[-2px] hover:shadow-xl active:translate-y-1 active:shadow-sm transition"
+            onClick={() => setIsQuizModalOpen(true)}
+          >
+            퀴즈 설정
+          </button>
+          <button
+            className="bg-blue-500 text-white font-bold py-2 px-4 rounded-md shadow-lg transform hover:translate-y-[-2px] hover:shadow-xl active:translate-y-1 active:shadow-sm transition"
+            onClick={handleStartGame}
+          >
+            게임 시작
+          </button>
+        </div>
+      )}
+
       <QuizSettingModal isOpen={isQuizModalOpen} onClose={() => setIsQuizModalOpen(false)} />
     </Card>
   );
