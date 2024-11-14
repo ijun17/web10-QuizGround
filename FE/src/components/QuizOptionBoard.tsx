acc5fb5629f0c94203a11b23e0851aa53c0ebe84
ijun17 <ijun17@naver.com>
2024-11-14 21:04:12 +0900

    feat: 퀴즈 정답 발표 시 보드에 정답 표시

--- FE/src/components/QuizOptionBoard.tsx
+++ FE/src/components/QuizOptionBoard.tsx
@@ -2,7 +2,7 @@ import { usePlayerStore } from '@/store/usePlayerStore';
 import { Player } from './Player';
 import { socketService } from '@/api/socket';
 import { useRoomStore } from '@/store/useRoomStore';
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useQuizeStore } from '@/store/useQuizStore';
 
 const optionColors = [
@@ -23,6 +23,8 @@ export const QuizOptionBoard = () => {
   const gameId = useRoomStore((state) => state.gameId);
   const players = usePlayerStore((state) => state.players);
   const options = useQuizeStore((state) => state.currentQuiz?.choiceList) || [];
+  const quizState = useQuizeStore((state) => state.quizState);
+  const quizAnswer = useQuizeStore((state) => state.currentAnswer);
   const [selectedOption, setSelectedOption] = useState(options.length);
 
   const handleClick: React.MouseEventHandler<HTMLDivElement> = (e) => {
@@ -36,30 +38,61 @@ export const QuizOptionBoard = () => {
     setSelectedOption(option);
   };
 
+  // const
+  const boardRef = useRef<HTMLDivElement | null>(null);
+  const [boardRect, setBoardRect] = useState<null | DOMRect>(null);
+
+  useEffect(() => {
+    if (boardRef.current) {
+      setBoardRect(boardRef.current.getBoundingClientRect());
+    }
+  }, []);
+
   return (
-    <div className="relative component-default h-[100%]" onClick={handleClick}>
+    <div
+      className="relative component-default h-[100%] select-none"
+      onClick={handleClick}
+      ref={boardRef}
+    >
       <div className="absolute h-[100%] w-[100%]">
-        {players.map((player) => (
-          <Player
-            key={player.playerId}
-            name={player.playerName}
-            position={player.playerPosition}
-            isCurrent={player.playerId === currentPlayerId}
-            isAnswer={player.isAnswer ?? false}
-          />
-        ))}
+        {boardRect
+          ? players.map((player) => {
+              return (
+                <Player
+                  key={player.playerId}
+                  name={player.playerName}
+                  position={[
+                    player.playerPosition[1] * boardRect.width,
+                    player.playerPosition[0] * boardRect.height
+                  ]}
+                  isCurrent={player.playerId === currentPlayerId}
+                  isAnswer={player.isAnswer ?? false}
+                />
+              );
+            })
+          : null}
       </div>
       <div className="grid grid-cols-2 gap-4 p-4 h-[100%] w-[100%]">
         {options.map((option, i) => (
           <div
-            className="rounded-lg flex justify-center items-center w-[100%] h-[100%]"
+            className={'rounded-lg flex justify-center items-center w-[100%] h-[100%]'}
             key={i}
             style={{
               background: optionColors[i],
-              border: 'solid 3px ' + (i === selectedOption ? 'lightgreen' : 'white')
+              border: 'solid 3px ' + (i === selectedOption ? 'lightgreen' : 'white'),
+              boxShadow:
+                quizState === 'end' && i === quizAnswer
+                  ? `
+      0 0 10px rgba(255, 215, 0, 0.8), 
+      0 0 20px rgba(255, 223, 0, 0.6), 
+      0 0 30px rgba(255, 223, 0, 0.5), 
+      0 0 40px rgba(255, 215, 0, 0.4)`
+                  : 'none',
+              opacity: quizState === 'end' && i !== quizAnswer ? '0.3' : '1',
+              textShadow: '-1px 0 white, 0 1px white, 1px 0 white, 0 -1px white'
             }}
           >
-            {i + 1 + '. ' + option.content}
+            <div className="z-10 font-bold text-lg text-black">{i + 1 + '. ' + option.content}</div>
           </div>
         ))}
       </div>
