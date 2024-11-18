7c8d18b06ca1712b4e842e4b3decea630daf39b3
ijun17 <ijun17@naver.com>
2024-11-19 01:42:04 +0900

    fix: 퀴즈 선택지 보드 버그 수정
    - 화면 크기 변경 시 플레이어 위치 재조정
    - 퀴즈 카운트 다운에는 선택지 보여주지 않음

--- FE/src/components/QuizOptionBoard.tsx
+++ FE/src/components/QuizOptionBoard.tsx
@@ -4,6 +4,7 @@ import { socketService } from '@/api/socket';
 import { useRoomStore } from '@/store/useRoomStore';
 import { useEffect, useRef, useState } from 'react';
 import { useQuizeStore } from '@/store/useQuizStore';
+import useServerDate from '@/hooks/useServerDate';
 
 const optionColors = [
   '#FF9AA2', // pastel red
@@ -22,10 +23,13 @@ export const QuizOptionBoard = () => {
   const currentPlayerId = usePlayerStore((state) => state.currentPlayerId);
   const gameId = useRoomStore((state) => state.gameId);
   const players = usePlayerStore((state) => state.players);
-  const options = useQuizeStore((state) => state.currentQuiz?.choiceList) || [];
+  const currentQuiz = useQuizeStore((state) => state.currentQuiz);
+  const choiceList = currentQuiz?.choiceList || [];
   const quizState = useQuizeStore((state) => state.quizState);
   const quizAnswer = useQuizeStore((state) => state.currentAnswer);
-  const [selectedOption, setSelectedOption] = useState(options.length);
+  const [selectedOption, setSelectedOption] = useState(currentQuiz?.choiceList.length);
+  const [choiceListVisible, setChoiceListVisible] = useState(false);
+  const serverNow = useServerDate();
 
   const handleClick: React.MouseEventHandler<HTMLDivElement> = (e) => {
     const { pageX, pageY } = e;
@@ -34,19 +38,21 @@ export const QuizOptionBoard = () => {
     const y = (pageY - top) / height;
     if (x > 1 || y > 1) return;
     socketService.emit('updatePosition', { gameId, newPosition: [y, x] });
-    const option = Math.round(x) + Math.floor(y * Math.ceil(options.length / 2)) * 2;
+    const option = Math.round(x) + Math.floor(y * Math.ceil(choiceList.length / 2)) * 2;
     setSelectedOption(option);
   };
 
   const boardRef = useRef<HTMLDivElement | null>(null);
   const [boardRect, setBoardRect] = useState<null | DOMRect>(null);
 
+  // 보드 크기 초기화
   useEffect(() => {
     if (boardRef.current) {
       setBoardRect(boardRef.current.getBoundingClientRect());
     }
   }, []);
 
+  // 화면 변화에 따라 보드 크기 재조정
   useEffect(() => {
     const handleResize = () => {
       if (boardRef.current) setBoardRect(boardRef.current.getBoundingClientRect());
@@ -56,6 +62,17 @@ export const QuizOptionBoard = () => {
       window.removeEventListener('resize', handleResize);
     };
   }, [boardRef]);
+
+  // 퀴즈 시작 시간에 선택지 렌더링
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (!choiceListVisible && currentQuiz && currentQuiz.startTime <= serverNow())
+        setChoiceListVisible(true);
+      else if (choiceListVisible && currentQuiz && currentQuiz.startTime > serverNow())
+        setChoiceListVisible(false);
+    }, 100);
+    return () => clearInterval(interval);
+  }, [choiceListVisible, currentQuiz, serverNow]);
 
   return (
     <div
@@ -82,28 +99,31 @@ export const QuizOptionBoard = () => {
           : null}
       </div>
       <div className="grid grid-cols-2 gap-4 p-4 h-[100%] w-[100%]">
-        {options.map((option, i) => (
-          <div
-            className={'rounded-lg flex justify-center items-center w-[100%] h-[100%]'}
-            key={i}
-            style={{
-              background: optionColors[i],
-              border: 'solid 3px ' + (i === selectedOption ? 'lightgreen' : 'white'),
-              boxShadow:
-                quizState === 'end' && i + 1 === quizAnswer
-                  ? `
+        {choiceListVisible &&
+          choiceList.map((option, i) => (
+            <div
+              className={'rounded-lg flex justify-center items-center w-[100%] h-[100%]'}
+              key={i}
+              style={{
+                background: optionColors[i],
+                border: 'solid 3px ' + (i === selectedOption ? 'lightgreen' : 'white'),
+                boxShadow:
+                  quizState === 'end' && option.order === quizAnswer
+                    ? `
       0 0 10px rgba(255, 215, 0, 0.8), 
       0 0 20px rgba(255, 223, 0, 0.6), 
       0 0 30px rgba(255, 223, 0, 0.5), 
       0 0 40px rgba(255, 215, 0, 0.4)`
-                  : 'none',
-              opacity: quizState === 'end' && i + 1 !== quizAnswer ? '0.3' : '1',
-              textShadow: '-1px 0 white, 0 1px white, 1px 0 white, 0 -1px white'
-            }}
-          >
-            <div className="z-10 font-bold text-lg text-black">{i + 1 + '. ' + option.content}</div>
-          </div>
-        ))}
+                    : 'none',
+                opacity: quizState === 'end' && option.order !== quizAnswer ? '0.3' : '1',
+                textShadow: '-1px 0 white, 0 1px white, 1px 0 white, 0 -1px white'
+              }}
+            >
+              <div className="z-10 font-bold text-lg text-black">
+                {i + 1 + '. ' + option.content}
+              </div>
+            </div>
+          ))}
       </div>
     </div>
   );
