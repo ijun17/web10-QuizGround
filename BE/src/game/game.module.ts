311cbab4d4a74d390c4a6a10c68f253d036eb48e
DongHoonYu96 <sbna154@gmail.com>
2024-11-19 15:24:41 +0900

    test: [BE] QuizSetService, test위치 변경후 환경설정

--- BE/src/game/game.module.ts
+++ BE/src/game/game.module.ts
@@ -5,18 +5,21 @@ import { RedisModule } from '@nestjs-modules/ioredis';
 import { GameValidator } from './validations/game.validator';
 import { GameChatService } from './service/game.chat.service';
 import { GameRoomService } from './service/game.room.service';
-import { QuizModule } from '../quiz/quiz.module';
 import { QuizCacheService } from './service/quiz.cache.service';
+import { QuizSetModule } from '../quiz-set/quiz-set.module';
+import { QuizSetService } from '../quiz-set/service/quiz-set.service';
 
 @Module({
-  imports: [RedisModule, QuizModule],
+  imports: [RedisModule, QuizSetModule],
   providers: [
     GameGateway,
     GameService,
     GameChatService,
     GameRoomService,
     GameValidator,
+    QuizSetService,
     QuizCacheService
-  ]
+  ],
+  exports: [GameService]
 })
 export class GameModule {}
--- BE/test/integration/game.integration.spec.ts
+++ BE/test/integration/game.integration.spec.ts
@@ -2,27 +2,30 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import { IoAdapter } from '@nestjs/platform-socket.io';
 import { io, Socket } from 'socket.io-client';
-import { GameGateway } from '../src/game/game.gateway';
-import { GameService } from '../src/game/service/game.service';
-import socketEvents from '../src/common/constants/socket-events';
-import { RedisModule } from '@nestjs-modules/ioredis';
+import { GameGateway } from '../../src/game/game.gateway';
+import { GameService } from '../../src/game/service/game.service';
+import socketEvents from '../../src/common/constants/socket-events';
 import { Redis } from 'ioredis';
-import { GameValidator } from '../src/game/validations/game.validator';
-import { GameChatService } from '../src/game/service/game.chat.service';
-import { GameRoomService } from '../src/game/service/game.room.service';
+import { GameValidator } from '../../src/game/validations/game.validator';
+import { GameChatService } from '../../src/game/service/game.chat.service';
+import { GameRoomService } from '../../src/game/service/game.room.service';
 import { HttpService } from '@nestjs/axios';
-import { mockQuizData } from './mocks/quiz-data.mock';
+import { mockQuizData } from '.././mocks/quiz-data.mock';
 import RedisMock from 'ioredis-mock';
-import { REDIS_KEY } from '../src/common/constants/redis-key.constant';
-import { QuizCacheService } from '../src/game/service/quiz.cache.service';
-import { QuizService } from '../src/quiz/quiz.service';
+import { REDIS_KEY } from '../../src/common/constants/redis-key.constant';
+import { QuizCacheService } from '../../src/game/service/quiz.cache.service';
+import { QuizSetService } from '../../src/quiz-set/service/quiz-set.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { QuizSetModel } from '../src/quiz/entities/quiz-set.entity';
-import { QuizModel } from '../src/quiz/entities/quiz.entity';
-import { QuizChoiceModel } from '../src/quiz/entities/quiz-choice.entity';
-import { UserModel } from '../src/user/entities/user.entity';
-import { UserQuizArchiveModel } from '../src/user/entities/user-quiz-archive.entity';
+import { QuizSetModel } from '../../src/quiz-set/entities/quiz-set.entity';
+import { QuizModel } from '../../src/quiz-set/entities/quiz.entity';
+import { QuizChoiceModel } from '../../src/quiz-set/entities/quiz-choice.entity';
+import { UserModel } from '../../src/user/entities/user.entity';
+import { UserQuizArchiveModel } from '../../src/user/entities/user-quiz-archive.entity';
 import { ConfigModule } from '@nestjs/config';
+import { QuizSetCreateService } from '../../src/quiz-set/service/quiz-set-create.service';
+import { QuizSetReadService } from '../../src/quiz-set/service/quiz-set-read.service';
+import { QuizSetUpdateService } from '../../src/quiz-set/service/quiz-set-update.service';
+import { QuizSetDeleteService } from '../../src/quiz-set/service/quiz-set-delete.service';
 
 const mockHttpService = {
   axiosRef: jest.fn().mockImplementation(() => {
@@ -84,7 +87,11 @@ describe('GameGateway (e2e)', () => {
         GameRoomService,
         GameValidator,
         QuizCacheService,
-        QuizService,
+        QuizSetService,
+        QuizSetCreateService,
+        QuizSetReadService,
+        QuizSetUpdateService,
+        QuizSetDeleteService,
         {
           provide: 'default_IORedisModuleConnectionToken',
           useValue: redisMock
@@ -504,7 +511,7 @@ describe('GameGateway (e2e)', () => {
         ]
       };
       const quizServiceSpy = jest
-        .spyOn(moduleRef.get(QuizService), 'findOne')
+        .spyOn(moduleRef.get(QuizSetService), 'findOne')
         .mockResolvedValue(mockQuizSet);
 
       // 6. 게임 시작
@@ -585,7 +592,7 @@ describe('GameGateway (e2e)', () => {
       );
 
       // 4. QuizService mock 설정 (호출되지 않아야 함)
-      const quizServiceSpy = jest.spyOn(moduleRef.get(QuizService), 'findOne');
+      const quizServiceSpy = jest.spyOn(moduleRef.get(QuizSetService), 'findOne');
 
       // 5. 게임 시작
       await new Promise<void>((resolve) => {
@@ -676,7 +683,7 @@ describe('GameGateway (e2e)', () => {
           }
         ]
       };
-      jest.spyOn(moduleRef.get(QuizService), 'findOne').mockResolvedValue(newQuizSet);
+      jest.spyOn(moduleRef.get(QuizSetService), 'findOne').mockResolvedValue(newQuizSet);
 
       // 5. 캐시 만료 대기
       await new Promise((resolve) => setTimeout(resolve, 1100));
