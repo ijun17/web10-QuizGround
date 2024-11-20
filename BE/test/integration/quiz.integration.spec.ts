aa94c5a864011ebf8b525155885ee3d3d9a3a3fa
songbuild00 <songbuild00@gmail.com>
2024-11-21 00:36:37 +0900

    test: imports 관련 오류 수정

--- BE/test/integration/quiz.integration.spec.ts
+++ BE/test/integration/quiz.integration.spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { DataSource, QueryRunner } from 'typeorm';
+import { DataSource, EntityManager, QueryRunner } from 'typeorm';
 import { BadRequestException } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { QuizSetService } from '../../src/quiz-set/service/quiz-set.service';
@@ -14,11 +14,17 @@ import { QuizSetCreateService } from '../../src/quiz-set/service/quiz-set-create
 import { QuizSetReadService } from '../../src/quiz-set/service/quiz-set-read.service';
 import { QuizSetUpdateService } from '../../src/quiz-set/service/quiz-set-update.service';
 import { QuizSetDeleteService } from '../../src/quiz-set/service/quiz-set-delete.service';
+import { UserService } from '../../src/user/user.service';
+import { JwtModule } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
 
 describe('QuizService', () => {
   let quizService: QuizSetService;
   let dataSource: DataSource;
   let queryRunner: QueryRunner;
+  let userService: UserService;
+  let testUser: UserModel;
+  let testOtherUser: UserModel;
 
   beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -43,25 +49,41 @@ describe('QuizService', () => {
           //   maxBatchSize: 100
           // }
         }),
-        TypeOrmModule.forFeature([QuizSetModel, QuizModel, QuizChoiceModel, UserModel])
+        TypeOrmModule.forFeature([QuizSetModel, QuizModel, QuizChoiceModel, UserModel]),
+        JwtModule
       ],
       providers: [
         QuizSetService,
         QuizSetCreateService,
         QuizSetReadService,
         QuizSetUpdateService,
-        QuizSetDeleteService
+        QuizSetDeleteService,
+        UserService
       ]
     }).compile();
 
     quizService = module.get<QuizSetService>(QuizSetService);
     dataSource = module.get<DataSource>(DataSource);
+    userService = module.get<UserService>(UserService);
   });
 
   beforeEach(async () => {
     queryRunner = dataSource.createQueryRunner();
     await queryRunner.connect();
     await queryRunner.startTransaction();
+
+    testUser = await createUser(
+      queryRunner.manager,
+      'integration_test@test.com',
+      'test_password',
+      'Test'
+    );
+    testOtherUser = await createUser(
+      queryRunner.manager,
+      'integration_other@test.comm',
+      'test_password',
+      'Test2'
+    );
   });
 
   afterEach(async () => {
@@ -76,6 +98,7 @@ describe('QuizService', () => {
   it('should be defined', () => {
     expect(quizService).toBeDefined();
     expect(dataSource).toBeDefined();
+    expect(userService).toBeDefined();
   });
 
   describe('퀴즈셋 생성 테스트', () => {
@@ -109,7 +132,7 @@ describe('QuizService', () => {
         ]
       };
 
-      const result = await quizService.createQuizSet(createQuizSetDto);
+      const result = await quizService.createQuizSet(createQuizSetDto, testUser);
       const savedQuizSet = await queryRunner.manager.findOne(QuizSetModel, {
         where: { id: result.id },
         relations: ['quizList', 'quizList.choiceList', 'user']
@@ -149,7 +172,7 @@ describe('QuizService', () => {
       };
 
       // When & Then
-      await expect(quizService.createQuizSet(invalidQuizSetDto)).rejects.toThrow(
+      await expect(quizService.createQuizSet(invalidQuizSetDto, testUser)).rejects.toThrow(
         BadRequestException
       );
     });
@@ -180,7 +203,7 @@ describe('QuizService', () => {
       };
 
       // When & Then
-      await expect(quizService.createQuizSet(duplicateOrderQuizSetDto)).rejects.toThrow(
+      await expect(quizService.createQuizSet(duplicateOrderQuizSetDto, testUser)).rejects.toThrow(
         BadRequestException
       );
     });
@@ -212,7 +235,7 @@ describe('QuizService', () => {
         ]
       };
 
-      await quizService.createQuizSet(createQuizSetDto);
+      await quizService.createQuizSet(createQuizSetDto, testUser);
 
       // When
       const result = await quizService.findAllWithQuizzesAndChoices('PROGRAMMING', 0, 10, '');
@@ -233,24 +256,24 @@ describe('QuizService', () => {
 
     it('검색어로 퀴즈셋 목록을 가져와야 한다', async () => {
       for (let i = 0; i < 20; i++) {
-        await createQuizSetTestData(quizService, `테스트${i}`);
+        await createQuizSetTestData(quizService, `테스트${i}`, testUser);
       }
 
       const result = await quizService.findAllWithQuizzesAndChoices('', 0, 10, '테스트19');
 
       expect(result.quizSetList).toHaveLength(1);
       expect(result.quizSetList[0].title).toBe('테스트19');
-    })
+    });
 
     it('검색어가 유효하지 않아 퀴즈셋 목록이 없다', async () => {
       for (let i = 0; i < 20; i++) {
-        await createQuizSetTestData(quizService, `테스트${i}`);
+        await createQuizSetTestData(quizService, `테스트${i}`, testUser);
       }
 
       const result = await quizService.findAllWithQuizzesAndChoices('', 0, 10, '테스트20');
 
       expect(result.quizSetList).toHaveLength(0);
-    })
+    });
   });
 
   describe('퀴즈셋 단일 조회 테스트', () => {
@@ -276,7 +299,7 @@ describe('QuizService', () => {
         ]
       };
 
-      const result = await quizService.createQuizSet(dto);
+      const result = await quizService.createQuizSet(dto, testUser);
       testQuizSet = result;
     });
 
@@ -320,7 +343,7 @@ describe('QuizService', () => {
         ]
       };
 
-      const result = await quizService.createQuizSet(dto);
+      const result = await quizService.createQuizSet(dto, testUser);
       originQuizSetId = result.id;
     });
 
@@ -345,7 +368,7 @@ describe('QuizService', () => {
       };
 
       // When
-      await quizService.update(originQuizSetId, updateDto);
+      await quizService.update(originQuizSetId, updateDto, testUser);
 
       // Then
       const updated = await quizService.findOne(originQuizSetId);
@@ -358,7 +381,13 @@ describe('QuizService', () => {
 
     it('존재하지 않는 퀴즈셋 수정시 에러가 발생해야 한다.', async () => {
       // When & Then
-      await expect(quizService.update(999999, { title: 'test' })).rejects.toThrow();
+      await expect(quizService.update(999999, { title: 'test' }, testUser)).rejects.toThrow();
+    });
+
+    it('생성한 사람과 다른 사람이 수정할 시 에러가 발생해야 한다.', async () => {
+      await expect(
+        quizService.update(originQuizSetId, { title: '수정' }, testOtherUser)
+      ).rejects.toThrow();
     });
   });
 
@@ -385,13 +414,13 @@ describe('QuizService', () => {
         ]
       };
 
-      const result = await quizService.createQuizSet(dto);
+      const result = await quizService.createQuizSet(dto, testUser);
       testQuizSet = result;
     });
 
     it('퀴즈셋을 soft delete 할 수 있어야 한다.', async () => {
       // When
-      const result = await quizService.remove(testQuizSet.id);
+      const result = await quizService.remove(testQuizSet.id, testUser);
 
       // Then
       expect(result.success).toBe(true);
@@ -402,12 +431,20 @@ describe('QuizService', () => {
 
     it('존재하지 않는 퀴즈셋 삭제시 에러가 발생해야 한다.', async () => {
       // When & Then
-      await expect(quizService.remove(999999)).rejects.toThrow();
+      await expect(quizService.remove(999999, testUser)).rejects.toThrow();
+    });
+
+    it('생성한 사람과 다른 사람이 삭제할 시 에러가 발생해야 한다.', async () => {
+      await expect(quizService.remove(testQuizSet.id, testOtherUser)).rejects.toThrow();
     });
   });
 });
 
-async function createQuizSetTestData(quizService: QuizSetService, quizSetTitle: string = '테스트') {
+async function createQuizSetTestData(
+  quizService: QuizSetService,
+  quizSetTitle: string = '테스트',
+  user: UserModel
+) {
   const createQuizSetDto: CreateQuizSetDto = {
     title: quizSetTitle,
     category: 'PROGRAMMING',
@@ -431,5 +468,24 @@ async function createQuizSetTestData(quizService: QuizSetService, quizSetTitle: 
     ]
   };
 
-  await quizService.createQuizSet(createQuizSetDto);
+  await quizService.createQuizSet(createQuizSetDto, user);
+}
+
+async function createUser(
+  manager: EntityManager,
+  email: string,
+  password: string,
+  nickname: string
+) {
+  const salt = await bcrypt.genSalt();
+  const hashedPassword = await bcrypt.hash(password, salt);
+  const newUser = manager.create(UserModel, {
+    email: email,
+    password: hashedPassword,
+    nickname: nickname,
+    status: '?'
+  });
+  await manager.save(newUser);
+
+  return newUser;
 }
