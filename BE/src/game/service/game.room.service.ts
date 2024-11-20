30d6b365a7dd25d240485b8e519fd931cc08100e
DongHoonYu96 <sbna154@gmail.com>
2024-11-21 01:45:06 +0900

    feat: [BE] 비활성 방 체크 주기적 실행
    방장이 잠수인경우, 명시적 퇴장이 없음

--- BE/src/game/service/game.room.service.ts
+++ BE/src/game/service/game.room.service.ts
@@ -9,11 +9,12 @@ import { JoinRoomDto } from '../dto/join-room.dto';
 import SocketEvents from '../../common/constants/socket-events';
 import { UpdateRoomOptionDto } from '../dto/update-room-option.dto';
 import { UpdateRoomQuizsetDto } from '../dto/update-room-quizset.dto';
+import { Cron, CronExpression } from '@nestjs/schedule';
 
 @Injectable()
 export class GameRoomService {
   private readonly logger = new Logger(GameRoomService.name);
-  private readonly INACTIVE_THRESHOLD = 30 * 60 * 1000; // 30분
+  private readonly INACTIVE_THRESHOLD = 30 * 60 * 1000; // 30분 30 * 60 * 1000;
   private readonly PLAYER_GRACE_PERIOD = 10; // 10초
 
   constructor(
@@ -177,16 +178,18 @@ export class GameRoomService {
   /**
    * 비활성 방 체크 (주기적으로 실행)
    */
+  @Cron(CronExpression.EVERY_MINUTE)
   async checkInactiveRooms(): Promise<void> {
     const now = Date.now();
     const rooms = await this.redis.smembers(REDIS_KEY.ACTIVE_ROOMS);
+    this.logger.verbose(`비활성 방 체크시작 / 활성 방 목록: ${rooms}`);
 
     for (const roomId of rooms) {
       const lastActivity = await this.redis.hget(REDIS_KEY.ROOM(roomId), 'lastActivityAt');
 
       if (lastActivity && now - parseInt(lastActivity) > this.INACTIVE_THRESHOLD) {
         await this.redis.publish('room:cleanup', roomId);
-        this.logger.log(`비활성으로 인해 방 ${roomId} 정리 시작`);
+        this.logger.verbose(`비활성으로 인해 방 ${roomId} 정리 시작`);
       }
     }
   }
