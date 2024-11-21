1eb2edad53f8028c280bc0b8098d2c4f16c57da7
ijun17 <ijun17@naver.com>
2024-11-21 18:13:04 +0900

    fix: 퀴즈 타입 수정

--- FE/src/api/rest/quizTypes.ts
+++ FE/src/api/rest/quizTypes.ts
@@ -24,7 +24,7 @@ export type QuizChoiceInput = {
 export type QuizInput = {
   quiz: string;
   limitTime: number;
-  choiceList: QuizChoiceInput[];
+  choices: QuizChoiceInput[];
 };
 
 export type CreateQuizSetPayload = {
