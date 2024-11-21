81ef717941100db2b2b312df341ccf3660452e63
ijun17 <ijun17@naver.com>
2024-11-21 18:07:30 +0900

    feat: pin 입력 페이지 개발

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -6,6 +6,7 @@ import { QuizSetupPage } from './pages/QuizSetupPage';
 import { GameLobbyPage } from './pages/GameLobbyPage';
 import { LoginPage } from './pages/LoginPage';
 import { MyPage } from './pages/MyPage';
+import { PinPage } from './pages/PinPage';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         <Route path="/game/lobby" element={<GameLobbyPage />} />
         <Route path="/quiz/setup" element={<QuizSetupPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/pin" element={<PinPage />} />
         <Route path="/mypage" element={<MyPage />} />
         <Route path="*" element={<div>not found</div>} />
       </Routes>
