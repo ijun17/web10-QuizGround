ff38ab1857977e9efb047117636ba832f4c5a216
ijun17 <ijun17@naver.com>
2024-11-28 01:15:47 +0900

    feat: 메인 페이지 로그인 모달 적용

--- FE/src/pages/InfoCard.tsx
+++ FE/src/pages/InfoCard.tsx
@@ -1,31 +1,40 @@
-import { Button } from '@mui/material';
+import Lottie from 'lottie-react';
 import { FC } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { AnimationConfigWithData } from 'lottie-web';
+import CustomButton from '@/components/CustomButton';
 
 type CardProps = {
   title: string;
-  description: string;
-  path?: string;
   action?: () => void;
+  path?: string;
+  icon: AnimationConfigWithData['animationData'];
 };
 
-export const InfoCard: FC<CardProps> = ({ title, description, path, action }) => {
+export const InfoCard: FC<CardProps> = ({ title, icon, path, action }) => {
   const navigate = useNavigate();
 
   return (
-    <div className="bg-white rounded-xl p-6 shadow-lg border-4 border-blue-200 flex flex-col justify-between">
+    <div className="bg-white rounded-xl p-2 shadow-lg border-4 border-blue-200 flex flex-col justify-between hover:scale-105 transition-all duration-300 animate-popup">
       <h3 className="text-xl font-semibold text-blue-600">{title}</h3>
-      <p className="mt-4 text-gray-600">{description}</p>
-      <Button
-        variant="outlined"
-        className="mt-4 w-full"
+      <Lottie
+        animationData={icon}
+        loop={true}
+        autoplay={true}
+        className="mx-auto cursor-pointer"
+        style={{
+          width: '125px',
+          height: '125px'
+        }}
+      />
+      <CustomButton
+        text={title}
+        className="w-full"
         onClick={() => {
           if (action) action();
           if (path) navigate(path);
         }}
-      >
-        {title}
-      </Button>
+      />
     </div>
   );
 };
