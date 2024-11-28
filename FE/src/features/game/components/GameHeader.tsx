8e517422d4d7f289dfec4b4dc84dc348db82ed1e
ijun17 <ijun17@naver.com>
2024-11-29 01:20:44 +0900

    feat: 나가기 버튼 추가

--- FE/src/features/game/components/GameHeader.tsx
+++ FE/src/features/game/components/GameHeader.tsx
@@ -1,16 +1,19 @@
 import { ClipboardCopy } from '../../../components/ClipboardCopy';
 import { QuizPreview } from '../../../components/QuizPreview';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { useRoomStore } from '@/features/game/data/store/useRoomStore';
 import React, { useState } from 'react';
 import { QuizSettingModal } from './QuizSettingModal';
 import { socketService } from '@/api/socket';
 import { usePlayerStore } from '@/features/game/data/store/usePlayerStore';
 import { useQuizStore } from '@/features/game/data/store/useQuizStore';
+import CustomButton from '@/components/CustomButton';
 
 export const GameHeader = React.memo(() => {
   const { gameId } = useParams<{ gameId: string }>();
-  const isHost = usePlayerStore((state) => state.isHost);
+  const players = usePlayerStore((state) => state.players);
+  const currentPlayerId = usePlayerStore((state) => state.currentPlayerId);
+  const isHost = players.get(currentPlayerId)?.isHost ?? false;
   const gameTitle = useRoomStore((state) => state.title);
   const [isQuizModalOpen, setIsQuizModalOpen] = useState(false);
   const { quizSetTitle, quizSetCategory } = useQuizStore();
@@ -20,9 +23,10 @@ export const GameHeader = React.memo(() => {
   const handleStartGame = () => {
     if (gameId) socketService.emit('startGame', { gameId });
   };
+  const navigate = useNavigate();
   // 예시
   return (
-    <div className="p-4 h-[100%] w-[100%]  component-popup">
+    <div className="p-4 h-[100%] w-[100%] component-popup">
       <div className="flex justify-center mb-4">
         <ClipboardCopy valueToCopy={pinNum} message={`PIN: ${pinNum} 복사`} />
         <ClipboardCopy valueToCopy={linkURL} message="공유 링크 복사" />
@@ -34,6 +38,7 @@ export const GameHeader = React.memo(() => {
         <div className="max-w-[500px] w-[100%]">
           <QuizPreview title={quizSetTitle} description={quizSetCategory} />
         </div>
+        <CustomButton text="나가기" size="small" color="yellow" onClick={() => navigate('/')} />
       </div>
       {isHost && (
         <div className="flex space-x-4 justify-center mt-4">
