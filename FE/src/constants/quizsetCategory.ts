5d8ed3dcde70fc03ce9c7cf108af15e430be516f
ijun17 <ijun17@naver.com>
2024-11-29 01:19:56 +0900

    feat: 퀴즈셋 카테고리 추가

---
+++ FE/src/constants/quizsetCategory.ts
@@ -0,0 +1,20 @@
+const QuizsetCategory = Object.freeze({
+  GENERAL: '상식',
+  MOVIE: '영화',
+  MUSIC: '음악',
+  FOOD: '음식',
+  ANIMAL: '동물',
+  LANGUAGE: '언어',
+  NEWS: '뉴스',
+  MATH: '수학',
+  SCIENCE: '과학',
+  ECONOMY: '경제',
+  HISTORY: '역사',
+  GEOGRAPHY: '지리',
+  SPORTS: '스포츠',
+  GAME: '게임',
+  IT: 'IT',
+  POLITICS: '정치'
+});
+
+export default QuizsetCategory;
