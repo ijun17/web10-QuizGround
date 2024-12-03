6f112c9763c972922e08094e7e5ed710675c908e
ijun17 <ijun17@naver.com>
2024-12-03 16:16:01 +0900

    fix: 소켓 목 변경

--- FE/src/api/socket/mocks/socketMocks/SocketMockHost.ts
+++ FE/src/api/socket/mocks/socketMocks/SocketMockHost.ts
@@ -10,5 +10,11 @@ export default class SocketMockHost extends SocketMock {
   async test() {
     await this.delay(1);
     this.emitServer('updateHost', { hostId: this.id });
+    this.emitServer('updateRoomOption', {
+      title: '즐거운 퀴즈 시간~~~~~~',
+      gameMode: 'SURVIVAL',
+      maxPlayerCount: 200,
+      isPublic: true
+    });
   }
 }
