e1bd9df9fe68c4364b32ae69d8ad34cbe7b5e19d
JunWoo Park <97427744+always97@users.noreply.github.com>
2024-12-04 23:50:57 +0900

    fix: 새로고침 수정

--- FE/src/features/lobby/LobbyList.tsx
+++ FE/src/features/lobby/LobbyList.tsx
@@ -35,7 +35,7 @@ export const LobbyList: React.FC<LobbyListProps> = ({ rooms, refreshRooms }) => 
             width: '50px',
             height: '50px'
           }}
-          onClick={() => refreshRooms}
+          onClick={refreshRooms}
         />
       </header>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 w-full">
